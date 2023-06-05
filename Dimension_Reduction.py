b1c775ed559080bb4bb2ae3c25250d6373315b65
AkashNagaraj <akash.nagaraj1@gmail.com>
2023-06-05 13:12:53 +0530

    final edits

--- Dimension_Reduction.py
+++ Dimension_Reduction.py
@@ -1,15 +1,15 @@
 ##https://github.com/coderoda/Cifar-10-Classification-using-scikit-learn/blob/master/smai-mini-project2.py
 
-import pickle, time, sys, os
+import pickle, time, sys, os, argparse, ast
 import numpy as np
 
-from sklearn import svm
-from sklearn.decomposition import PCA
-from sklearn import manifold
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import euclidean_distances
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+
+from sklearn import svm, manifold
+from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 
@@ -41,7 +41,7 @@ def load_cifar():
 
 def PCA_main(X_new):
   start = time.time()
-  pca = PCA(n_components = 192)
+  pca = PCA(n_components = 5)
   end = time.time()
   X_final = pca.fit_transform(X_new)
   print("Number of seconds to perform PCA decomposition : ",end-start)
@@ -56,7 +56,7 @@ def MDS_main(X_new):
     n_components=6,
     metric=False,
     max_iter=1000,
-    eps=1e-8,
+    eps=1e-4,
     dissimilarity="precomputed",
     random_state=5,
     n_jobs=1,
@@ -88,7 +88,7 @@ def tsne_main(X):
 
 
 def KNN_classifier(X,y,dim_reduction):
-    neighbor = [20,30,40]#,50,60,70,80,90,100]
+    neighbor = [1,5,10,20,30,40,50,60,70,80,90,100]
     
     if dim_reduction:
         X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
@@ -162,16 +162,19 @@ def store_data(cifar_trn_data, cifar_trn_labels, cifar_tst_data, cifar_tst_label
 
 
 def load_data():
-    global X_train,y_train,X_test,y_test
     X_train = np.load("data/X_train.npy")
     y_train = np.load("data/y_train.npy")
     X_test = np.load("data/X_test.npy")
     y_test = np.load("data/y_test.npy")
+    return X_train,X_test,y_train,y_test
 
 
-def main():
+def main(sample_size, classification_model, dimension_reduction):
     
-    total_size, sample_size = 50000, 1000
+    classification_model = classification_model.split(",")
+    dimension_reduction = dimension_reduction.split(",")
+
+    total_size = 50000
     build_cifar = False
     dim_reduction = True
 
@@ -179,52 +182,44 @@ def main():
         cifar_trn_data, cifar_trn_labels, cifar_tst_data, cifar_tst_labels = load_cifar()
         store_data(cifar_trn_data, cifar_trn_labels, cifar_tst_data, cifar_tst_labels)
     else:
-        load_data()
-    
+        X_train,X_test,y_train,y_test = load_data()
+ 
     print("The size of the data being used is : ",sample_size)
     
-    """ 
-    KNN_classifier(X_train[:sample_size],y_train[:sample_size],False)
-    SVM_classifier(X_train[:sample_size],y_train[:sample_size],False) 
-    LDA_classifier(X_train[:sample_size],y_train[:sample_size],False) 
-    sys.exit()
-    """
-
-    print("Running different dimension reduction algorithms")
-    
-    """
-    X_pca = PCA_main(X_test)
-    with open("data/pca/X_pca_test.npy","wb") as f:
-        np.save(f,X_pca)
-    """
-    X_tsne = tsne_main(X_train[:sample_size])
-    with open("data/tsne/X_tsne_"+str(sample_size)+".npy","wb") as f:
-        np.save(f,X_tsne)
-    """
-    X_mds = MDS_main(X_train[:sample_size])
-    with open("data/mds/X_mds_"+str(sample_size)+".npy","wb") as f:
-        np.save(f,X_mds)
-    """
-    #X_pca = np.load("data/pca/X_pca_"+str(sample_size)+".npy")
-    #X_tsne = np.load("data/tsne/X_tsne_"+str(sample_size)+".npy")
-    #X_mds = np.load("data/mds/X_mds_"+str(sample_size)+".npy")
-
-    print("Starting KNN")
-    #KNN_classifier(X_pca,y_train[:sample_size],dim_reduction=True)
-    KNN_classifier(X_tsne,y_train[:sample_size],dim_reduction=True)
-    #KNN_classifier(X_mds,y_train[:sample_size],dim_reduction=True)
-    
-
-    print("Starting SVM")
-    SVM_classifier(X_pca,y_train[:sample_size],dim_reduction=True)
-    #SVM_classifier(X_tsne,y_train[:sample_size],dim_reduction=True)
-    #SVM_classifier(X_mds,y_train[:sample_size],dim_reduction=True)
-    
-    print("Starting LDA")
-    LDA_classifier(X_pca,y_train[:sample_size],dim_reduction=True)
-    #LDA_classifier(X_tsne,y_train[:sample_size],dim_reduction=True)
-    #LDA_classifier(X_mds,y_train[:sample_size],dim_reduction=True) 
-    
+    choice = input("Classifiy the data directly? [Y/N] ")
+    if choice=="Y":
+        KNN_classifier(X_train[:sample_size],y_train[:sample_size],False)
+        SVM_classifier(X_train[:sample_size],y_train[:sample_size],False) 
+        LDA_classifier(X_train[:sample_size],y_train[:sample_size],False) 
+    else:
+        #print("Running {} dimension reduction algorithms.".format(dimension_reduction))
+        for val in dimension_reduction:
+            print("Running dimension reduction algorithm : ",val)
+            dim_query = val+"_main"
+            func1 = globals()[dim_query]
+            X = func1(X_train[:sample_size])
+            writing_dir = "data/"+dim_query.split("_")[0] + "/X"+"_"+dim_query.split("_")[0]+"_"+str(sample_size)+".npy"
+            with open(writing_dir,"wb") as f:
+                np.save(f,X)
+            
+            # Running classificaiton models
+            for val in classification_model:
+                print("Running classification model :",val)
+                model_query = val+"_classifier"
+                func2 = globals()[model_query]
+                func2(X,y_train[:sample_size],True)
+                
 
 if __name__=="__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--size",type=int,default=500,help="The size of the data you want to use")
+    parser.add_argument("--classification",type=str,default='KNN,SVM',help="Enter the models you want to use")
+    parser.add_argument("--dim_reduction",type=str,default='PCA,tSNE,NMDS',help="Enter the dimension reduction technique you want to implement")
+        
+    args=parser.parse_args()
+    sample_size = args.size
+    classification_model = args.classification
+    dimension_reduction = args.dim_reduction
+    
+    main(sample_size,classification_model,dimension_reduction)
+
--- visualization.py
+++ visualization.py
@@ -1,16 +1,63 @@
 import pandas as pd
 import seaborn as sns
+import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
+import plotly.express as px
 
-
-def plots():
-    pca_data = [62.133, 61.0667, 60, 60.533, 60.8, 59.466, 57.333, 57.33, 56.533, 55.466]
-    nmds_data = [25.066,22.933,24.5353,24.5353,24.8,24,21.866,24,25.066,22.666]
-    K_values = [10,20,30,40,50,60,70,80,90,100]
+def KNN_variations():
+    pca_data = [25,23.5,24.5,27,28,27,27.5,27,26.5,25,25.5,25]
+    nmds_data = [5,9.5,9,11.5,10,9,9.5,9.5,8.5,11,10,8.5]
+    K_values = [1,5,10,20,30,40,50,60,70,80,90,100]
 
     df = pd.DataFrame({'PCA': pca_data,'NMDS': nmds_data,'K': K_values})
+    fig = px.line(df,x="K",y=df.columns[0:2],markers=True,title="Accuracy score for different nearest neighbors")
+    fig.update_layout(
+        font_family="Courier New",
+        title_x=0.5,
+        title_font_family="Times New Roman",
+        title_font_color="rgb(10,29,81)",
+        yaxis_title="Accuracy %",
+        xaxis_title="# Neighbors",
+    )
 
-    sns.lineplot(data=df,x="K",y="PCA")
+    fig.write_image("../Dimension_Reduction_IEEE/PCA vs NMDS.png")
+
+def accuracy():
+    KNN = [22.6,28.36,29.55,31.34,32.79]#,29.86]
+    SVM = [34.38,43.86,47.14,50.39,51.68]
+    LDA = [18.5,18.98,26.03,32.05,34.64]
+    image_size = [1000,5000,10000,20000,30000]#,50000]
+
+    data = pd.DataFrame({"KNN":KNN,"SVM":SVM,"LDA":LDA,"IMAGE":image_size})
+    sns.set_style("darkgrid")
+    sns.lineplot(data=data,x="IMAGE",y="KNN",label="KNN").set(title="Accuracy Scores",xlabel="Number of training images",ylabel="Accuracy %")
+    sns.lineplot(data=data,x="IMAGE",y="SVM",label="SVM")
+    sns.lineplot(data=data,x="IMAGE",y="LDA",label="LDA")
+    plt.savefig("../Dimension_Reduction_IEEE/Accuracy Scores.png")
+
     plt.show()
 
-plots()
+
+# https://stackoverflow.com/questions/12487060/matplotlib-color-according-to-class-labels
+def tsne_plot():
+    X = np.load("data/tsne/X_tsne_5000.npy")
+    label = np.load("data/y_train.npy")[:5000]
+    colors= ["navy","darkblue","slateblue","rebeccapurple","indigo","darkmagenta","crimson","lightseagreen","yellow","darkorange"]
+    plt.style.use('dark_background')    
+    fig = plt.figure(figsize=(10,10))
+    plt.scatter(X[:,0],X[:,1],c=label,cmap=matplotlib.colors.ListedColormap(colors))
+    plt.title("Resulting t-SNE projection")
+
+    cb = plt.colorbar()
+    loc = np.arange(0,max(label),max(label)/float(len(colors)))
+    cb.set_ticks(loc)
+    #cb.set_ticklabels(colors) 
+    plt.savefig("../Dimension_Reduction_IEEE/tsne_projection.png")
+    plt.show()
+    
+    
+
+tsne_plot()
+#accuracy()
+#KNN_variations()
